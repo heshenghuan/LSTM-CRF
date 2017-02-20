547601b14ed0f210e560fa97dc7e9163ab8e9455
heshenghuan <heshenghuan@sina.com>
2017-02-20 18:03:28 +0800

    __init__ file

---
+++ keras_src/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Created on 2017-02-20 16:16:36
+
+@author: heshenghuan (heshenghuan@sina.com)
+http://github.com/heshenghuan
+"""
+
+__all__ = ['lstm_ner', 'neural_lib', 'train_util', 'evaluate_util']
