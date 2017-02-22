c8c0028e6f05cbff48fb4a3d767de0cac4c4598d
heshenghuan <heshenghuan@sina.com>
2017-02-22 22:23:11 +0800

    module constant, contain some constans value

---
+++ keras_src/constant.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Created on 2017-02-22 21:05:01
+
+@author: heshenghuan (heshenghuan@sina.com)
+http://github.com/heshenghuan
+
+Some constants may need to use.
+"""
+
+
+OOV = '_OOV_'
+GOLD_TAG = 'GoldNER'
+POS = 'POS'
+SEG = 'Segmentation'
+PRED_TAG = 'NER'
+task = 'ner'
+
+# Feature templates
+local_templates = (
+    (('w', -2), ),
+    (('w', -1), ),
+    (('w',  0), ),
+    (('w',  1), ),
+    (('w',  2), ),
+    (('w', -2), ('w',  -1)),
+    (('w', -1), ('w',  0)),
+    (('w',  0), ('w',  1)),
+    (('w',  1), ('w',  2)),
+    (('w',  -1), ('w',  1)),
+)
