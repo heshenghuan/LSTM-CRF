6c7b63e9f43c5b82ff80c82a92a2c706c52be839
heshenghuan <heshenghuan@sina.com>
2017-09-12 14:29:29 +0800

    update: process lib 0.1.2

---
+++ lib/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 08 2017
+
+@author: Heshenghuan (heshenghuan@sina.com)
+http://github.com/heshenghuan
+"""
+
+__all__ = [
+    "features", "parameters",
+    "utils", "pretreatment"
+]
+
+__version__ = 'NeuralTextProcess lib 0.1.2'
