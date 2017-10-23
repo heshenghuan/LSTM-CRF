2263882976293a397ba8466e2dc0597e7ceeb86e
heshenghuan <heshenghuan@sina.com>
2017-10-23 09:32:32 +0800

    Added submodule of lib

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ import numpy as np
 import codecs as cs
 import tensorflow as tf
 import model as tagger
-from lib.parameters import MAX_LEN
-from lib.features import Template
-from lib.utils import eval_ner, read_emb_from_file
-from lib.pretreatment import pretreatment, unfold_corpus, conv_corpus, read_corpus
+from lib.src.parameters import MAX_LEN
+from lib.src.features import Template
+from lib.src.utils import eval_ner, read_emb_from_file
+from lib.src.pretreatment import pretreatment, unfold_corpus, conv_corpus, read_corpus
 from env_settings import MODEL_DIR, DATA_DIR, EMB_DIR, OUTPUT_DIR, LOG_DIR
 
 
